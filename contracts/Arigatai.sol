8386e3c203524d50cea24ca0183f61a24dca63fb
Panos Matsinopoulos <panos@mixlr.com>
2024-09-23 08:32:20 +0300

    First version of Arigatai based on OpenZeppelin contracts

---
+++ contracts/Arigatai.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.27;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+string constant NAME = "Arigatai";
+string constant SYMBOL = "ARIG";
+
+contract Arigatai is ERC20, ERC20Pausable, Ownable {
+    constructor(
+        address initialOwner
+    ) ERC20(NAME, SYMBOL) Ownable(initialOwner) {}
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal override(ERC20, ERC20Pausable) {
+        super._update(from, to, value);
+    }
+}
